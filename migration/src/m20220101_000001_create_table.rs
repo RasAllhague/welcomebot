fd15c31ddb4f2fa56ffc24105837b5c08f361fa6
RasAllhague <50292283+RasAllhague@users.noreply.github.com>
2024-09-10 11:38:13 +0200

    fix down migration

--- migration/src/m20220101_000001_create_table.rs
+++ migration/src/m20220101_000001_create_table.rs
@@ -118,15 +118,15 @@ impl MigrationTrait for Migration {
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         manager
-            .drop_table(Table::drop().table(Guild::Table).to_owned())
+            .drop_table(Table::drop().if_exists().table(Guild::Table).to_owned())
             .await?;
 
         manager
-            .drop_table(Table::drop().table(Image::Table).to_owned())
+            .drop_table(Table::drop().if_exists().table(Image::Table).to_owned())
             .await?;
 
         manager
-            .drop_table(Table::drop().table(Image::Table).to_owned())
+            .drop_table(Table::drop().if_exists().table(WelcomeSettings::Table).to_owned())
             .await?;
 
         Ok(())
