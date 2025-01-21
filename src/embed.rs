e141a29d0d88c9287e1205c8bc14d22cb4398599
RasAllhague <50292283+RasAllhague@users.noreply.github.com>
2025-01-21 11:46:01 +0100

    add icon url

--- src/embed.rs
+++ src/embed.rs
@@ -3,15 +3,17 @@ use poise::serenity_prelude::{self as serenity, Color, CreateEmbedAuthor, Timest
 pub struct BanEmbed {
     pub banned_id: i64,
     pub banned_name: String,
+    pub banned_icon_url: String,
     pub reason: String,
     pub bot_name: String,
 }
 
 impl BanEmbed {
-    pub fn new(banned_id: i64, banned_name: String, reason: String, bot_name: String) -> Self {
+    pub fn new(banned_id: i64, banned_name: String, banned_icon_url: String, reason: String, bot_name: String) -> Self {
         Self {
             banned_id,
             banned_name,
+            banned_icon_url,
             reason,
             bot_name,
         }
@@ -22,7 +24,7 @@ impl BanEmbed {
             .title(format!("User banned: {}", self.banned_name))
             .description(format!("Banned for: {}", self.reason))
             .field("Id", self.banned_id.to_string(), false)
-            .author(CreateEmbedAuthor::new(&self.bot_name))
+            .author(CreateEmbedAuthor::new(&self.bot_name).icon_url(&self.banned_icon_url))
             .color(Color::RED)
             .timestamp(Timestamp::now())
     }
