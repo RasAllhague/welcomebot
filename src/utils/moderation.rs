ae680c417401971a7ba7031d0ae1c3fdbd37be6f
RasAllhague <50292283+RasAllhague@users.noreply.github.com>
2025-01-21 10:43:28 +0100

    refactoring 5

--- src/utils/moderation.rs
+++ src/utils/moderation.rs
@@ -11,36 +11,40 @@ pub async fn ban_member_if_contains_autoban(
     member: &serenity::Member,
     event: &serenity::GuildMemberUpdateEvent,
 ) -> Result<bool, PoiseError> {
-    if let Some(role_id) = guild.auto_ban_role_id {
-        if event.roles.iter().any(|x| x.get() as i64 == role_id) {
-            let ban_reason = guild
-                .ban_reason_template
-                .clone()
-                .unwrap_or("Banned due to choosing auto ban role.".to_string());
+    let role_id = match guild.auto_ban_role_id {
+        Some(role_id) => role_id,
+        None => return Ok(false),
+    };
+    
+    if event.roles.iter().any(|x| x.get() as i64 == role_id) {
+        let ban_reason = guild
+            .ban_reason_template
+            .clone()
+            .unwrap_or("Banned due to choosing auto ban role.".to_string());
 
-            match member.ban_with_reason(&ctx, 7, ban_reason).await {
-                Ok(_) => {
-                    warn!(
-                        "User banned: Id:'{}', name:'{}'.",
-                        member.user.id,
-                        member.display_name()
-                    );
+        match member.ban_with_reason(&ctx, 7, ban_reason).await {
+            Ok(_) => {
+                warn!(
+                    "User banned: Id:'{}', name:'{}'.",
+                    member.user.id,
+                    member.display_name()
+                );
 
-                    return Ok(true);
-                }
-                Err(why) => {
-                    error!(
-                        "Could not ban: Id:'{}', name:'{}', because: {}",
-                        member.user.id,
-                        member.display_name(),
-                        why
-                    );
+                return Ok(true);
+            }
+            Err(why) => {
+                error!(
+                    "Could not ban: Id:'{}', name:'{}', because: {}",
+                    member.user.id,
+                    member.display_name(),
+                    why
+                );
 
-                    return Ok(false);
-                }
+                return Ok(false);
             }
         }
     }
+
     Ok(false)
 }
 
